cb0dc3ffcb3cecba7dc71e3033870cb856ff4168
Jael Tinman <jaeltg@hotmail.com>
2021-08-05 13:26:24 +0100

    shows pub address

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import Request from './helpers/request';
-
 import {GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api'
 import mapStyle from './mapStyle.js';
 
@@ -27,6 +26,7 @@ function App() {
   const [allPubLocations, setAllPubLocations] = useState([]);
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [shotScope, setShotScope] = useState(null);
+  const [selectedAddress, setSelectedAddress] = useState(null)
 
   useEffect(() => {
     const request = new Request();
@@ -35,7 +35,18 @@ function App() {
     .then((data) => {
         setAllPubLocations(data)
     })
-    }, []) 
+  }, []) 
+
+  useEffect(() => {
+    if (selectedLocation) {
+    const request = new Request();
+
+    request.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${selectedLocation.lat},${selectedLocation.lng}&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`)
+    .then((data) => {
+        setSelectedAddress(data.results[0].formatted_address)
+    })}
+
+  }, [selectedLocation])
 
   const {isLoaded, loadError} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
@@ -45,7 +56,8 @@ function App() {
   if (!isLoaded) return "Loading..."
 
 
-  return (
+  if (isLoaded) {
+    return (
     <div className="App">
       <h1 className="Title">PUB PLOTTER</h1>
       <GoogleMap 
@@ -62,7 +74,7 @@ function App() {
             url: '/icons/beer3.svg',
             scaledSize: new window.google.maps.Size(40, 40),
             origin: new window.google.maps.Point(0, 0),
-            anchor: new window.google.maps.Point(15, 15),
+            anchor: new window.google.maps.Point(20, 20),
           }}
           onClick={() => {setSelectedLocation(pubLocation)}}
           />
@@ -79,21 +91,23 @@ function App() {
         />
 
         {selectedLocation ? (<InfoWindow position={{lat: selectedLocation.lat, lng: selectedLocation.lng}} onCloseClick={() => {setSelectedLocation(null)}}>
-          <div>
+          <div className="visited">
             <h3>Pub Visited &#10004;</h3>
+            <p>{selectedAddress}</p>
           </div>
         </InfoWindow>) : null}
 
        {shotScope ? (<InfoWindow position={{lat:55.93671, lng:-3.13809}} onCloseClick={() => {setShotScope(null)}}>
-          <div className="HIO">
+          <div className="message">
             <p>Hope my Pub Plotter is a...</p>
             <img src={require("./holeInOne.gif").default} alt="Hole in One"/>
-            <h3>Hole in One!</h3>
+            <h3 className="HIO">Hole in One!</h3>
           </div>
         </InfoWindow>) : null}
       </GoogleMap>
     </div>
   )
+ }
 }
 
 export default App;
